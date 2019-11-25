c7cac9cb3eec220f4eba2ecc1b3c71da901e4711
noothanprem <noothanprem@gmail.com>
2019-11-25 17:28:25 +0530

    refactor : changed hardcoded things

--- gambler.py
+++ gambler.py
@@ -2,24 +2,25 @@ import random
 class Gambler:
 	STARTING_STAKE_AMOUNT = 100
 	bet_amount = 1
-	daily_stake_amount = 100 
+	daily_stake_amount = STARTING_STAKE_AMOUNT
 	won_count=lose_count=0
 	win_percentage=lose_percentage=0
 	def win_or_lose(self):
-		self.daily_stake_amount=100
-		self.win_percentage = self.STARTING_STAKE_AMOUNT + (self.STARTING_STAKE_AMOUNT/2)
-		self.lose_percentage = self.STARTING_STAKE_AMOUNT / 2
-		self.won_count=self.lose_count=0
+		self.daily_stake_amount=STARTING_STAKE_AMOUNT
+		half_percentage = (self.STARTING_STAKE_AMOUNT*50)/100
+		self.win_percentage = self.STARTING_STAKE_AMOUNT + half_percentage
+		self.lose_percentage = self.STARTING_STAKE_AMOUNT - half_percentage
 		print("daily stake amount at first: ",self.daily_stake_amount)
 		print("win percentage at first : ",self.win_percentage)
 		print("Lose percentage : ",self.lose_percentage)
 		while(self.daily_stake_amount < self.win_percentage and self.daily_stake_amount > self.lose_percentage):
 			print("Inside while loop")
 			random_number = random.random()
+			bet_amount = 1
 			if(random_number < 0.5):
-				self.daily_stake_amount += 1
+				self.daily_stake_amount += bet_amount
 			else:
-				self.daily_stake_amount -=1
+				self.daily_stake_amount -= bet_amount
 			print("current stake : ",self.daily_stake_amount)
 		print("End of the day")	
 		self.win_percentage=self.lose_percentage=0
@@ -34,14 +35,11 @@ class Gambler:
 			
 			final_stake_amount = self.win_or_lose()
 			print("stake for the day : " ,final_stake_amount)
-			#if(final_stake_amount > 100):
-			amount_difference = final_stake_amount - 100
+	
+			amount_difference = final_stake_amount - STARTING_STAKE_AMOUNT
 			print("amount difference : ",amount_difference)
 			total_amount += amount_difference
 				
-			#else:
-				#total_amount -= final_stake_amount 
-				#print("You lose :",final_stake_amount)
 			print("day :",day_count)
 			days_list.append(total_amount)
 			day_count += 1
